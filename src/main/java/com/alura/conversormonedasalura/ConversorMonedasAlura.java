ccd7a6d1fae6482727bc11a9b16434479619216e
Nelkha <maritogallego@hotmail.com>
2024-09-16 23:52:21 -0300

    Primer commit

---
+++ src/main/java/com/alura/conversormonedasalura/ConversorMonedasAlura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alura.conversormonedasalura;
+
+/**
+ *
+ * @author Nelkha
+ */
+public class ConversorMonedasAlura {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo , este es mi primer programa, ya soy programador");
+    }
+}
